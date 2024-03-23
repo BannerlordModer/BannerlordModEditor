935d1a4460c26eab9d8ee70cfadb5e10899a8cd6
ModerRAS <adqewsx@gmail.com>
2024-03-23 23:19:55 +0800

    使用`Velopack`实现Github内更新

--- BannerlordModEditor/Program.cs
+++ BannerlordModEditor/Program.cs
@@ -1,10 +1,37 @@
+﻿using Microsoft.VisualBasic.Logging;
+using Velopack;
+using Velopack.Sources;
+
 namespace BannerlordModEditor {
     internal static class Program {
+        private static async Task UpdateMyApp() {
+            var mgr = new UpdateManager(new GithubSource("https://github.com/ModerRAS/WeMeetRecorder", null, false));
+
+            // check for new version
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion == null)
+                return; // no update available
+
+
+            // download new version
+            await mgr.DownloadUpdatesAsync(newVersion);
+
+            // install new version and restart app
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
+            VelopackApp.Build().Run();
+            Task.Run(async () => {
+                try {
+                    await UpdateMyApp();
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.ToString());
+                }
+            });
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
