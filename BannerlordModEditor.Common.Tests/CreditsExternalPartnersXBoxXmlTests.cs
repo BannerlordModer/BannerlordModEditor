f0d34391850829ce4770785b824709084397b38f
ModerRAS <adqewsx@gmail.com>
2025-06-24 10:35:49 +0800

    feat: Adapt CreditsExternalPartnersXBox.xml

---
+++ BannerlordModEditor.Common.Tests/CreditsExternalPartnersXBoxXmlTests.cs
@@ -0,0 +1,56 @@
+using BannerlordModEditor.Common.Models.Data;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace BannerlordModEditor.Common.Tests
+{
+    public class CreditsExternalPartnersXBoxXmlTests
+    {
+        [Fact]
+        public void CreditsExternalPartnersXBox_LoadAndSave_ShouldBeLogicallyIdentical()
+        {
+            var xmlPath = Path.Combine(TestUtils.GetSolutionRoot(), "BannerlordModEditor.Common.Tests", "TestData", "CreditsExternalPartnersXBox.xml");
+
+            // Deserialization using the existing model
+            var serializer = new XmlSerializer(typeof(CreditsLegalConsole));
+            CreditsLegalConsole? model;
+            using (var fileStream = new FileStream(xmlPath, FileMode.Open))
+            {
+                model = serializer.Deserialize(fileStream) as CreditsLegalConsole;
+            }
+
+            Assert.NotNull(model);
+
+            // Serialization
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "\t",
+                Encoding = new UTF8Encoding(false),
+                OmitXmlDeclaration = false
+            };
+
+            string serializedXml;
+            using (var stringWriter = new StringWriter())
+            using (var xmlWriter = XmlWriter.Create(stringWriter, settings))
+            {
+                var ns = new XmlSerializerNamespaces();
+                ns.Add("", "");
+                serializer.Serialize(xmlWriter, model, ns);
+                serializedXml = stringWriter.ToString();
+            }
+
+            // Comparison
+            var originalXml = File.ReadAllText(xmlPath, Encoding.UTF8);
+
+            var originalDoc = XDocument.Parse(originalXml);
+            var serializedDoc = XDocument.Parse(serializedXml);
+
+            Assert.True(XNode.DeepEquals(originalDoc, serializedDoc), "The XML was not logically identical after a round-trip.");
+        }
+    }
+} 
