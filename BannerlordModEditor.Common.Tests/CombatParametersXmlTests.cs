33d1a3b11793458b4f9d46e1e7b4474bdea14eda
ModerRAS <adqewsx@gmail.com>
2025-08-06 09:36:57 +0000

    适配 ModuleData XML 文件为强类型对象，添加对应 xUnit 测试，确保精确序列化行为

--- BannerlordModEditor.Common.Tests/CombatParametersXmlTests.cs
+++ BannerlordModEditor.Common.Tests/CombatParametersXmlTests.cs
@@ -21,5 +21,22 @@ namespace BannerlordModEditor.Common.Tests
             // 结构化对比
             Assert.True(XmlTestUtils.AreStructurallyEqual(xml, xml2));
         }
+
+        [Theory]
+        [InlineData("BannerlordModEditor.Common.Tests/TestData/combat_parameters_part1.xml")]
+        [InlineData("BannerlordModEditor.Common.Tests/TestData/combat_parameters_part2.xml")]
+        public void CombatParameters_PartFiles_RoundTrip_StructuralEquality(string xmlPath)
+        {
+            var xml = File.ReadAllText(xmlPath);
+
+            // 反序列化
+            var obj = XmlTestUtils.Deserialize<CombatParameters>(xml);
+
+            // 再序列化
+            var xml2 = XmlTestUtils.Serialize(obj);
+
+            // 结构化对比
+            Assert.True(XmlTestUtils.AreStructurallyEqual(xml, xml2));
+        }
     }
 }
