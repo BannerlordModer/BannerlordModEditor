4e88f17a2324e570789e8601e2758d3ac2eaa9d5
ModerRAS <adqewsx@gmail.com>
2025-08-06 09:42:01 +0000

    适配 ModuleData XML 文件为强类型对象，添加对应 xUnit 测试，确保精确序列化行为

--- BannerlordModEditor.Common.Tests/CraftingTemplatesXmlTests.cs
+++ BannerlordModEditor.Common.Tests/CraftingTemplatesXmlTests.cs
@@ -1,36 +1,21 @@
-using BannerlordModEditor.Common.Models.Game;
-using System;
 using System.IO;
-using System.Linq;
-using System.Xml.Serialization;
 using Xunit;
+using BannerlordModEditor.Common.Models.Data;
 
 namespace BannerlordModEditor.Common.Tests
 {
     public class CraftingTemplatesXmlTests
     {
-        private static string FindSolutionRoot()
+        [Theory]
+        [InlineData("TestData/crafting_templates.xml")]
+        [InlineData("TestData/crafting_templates_part1.xml")]
+        [InlineData("TestData/crafting_templates_part2.xml")]
+        public void CraftingTemplates_RoundTrip_StructuralEquality(string filePath)
         {
-            var directory = new DirectoryInfo(AppContext.BaseDirectory);
-            while (directory != null && !directory.GetFiles("*.sln").Any())
-            {
-                directory = directory.Parent;
-            }
-            return directory?.FullName ?? throw new DirectoryNotFoundException("Solution root not found");
-        }
-
-        [Fact]
-        public void CraftingTemplates_Deserialization_Works()
-        {
-            var solutionRoot = FindSolutionRoot();
-            var xmlPath = Path.Combine(solutionRoot, "BannerlordModEditor.Common.Tests", "TestData", "crafting_templates.xml");
-            var serializer = new XmlSerializer(typeof(CraftingTemplates));
-            using var fileStream = new FileStream(xmlPath, FileMode.Open);
-            var result = serializer.Deserialize(fileStream) as CraftingTemplates;
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.CraftingTemplateList);
-            Assert.True(result.CraftingTemplateList.Any());
+            var xml = File.ReadAllText(filePath);
+            var obj = XmlTestUtils.Deserialize<CraftingTemplates>(xml);
+            var xml2 = XmlTestUtils.Serialize(obj);
+            Assert.True(XmlTestUtils.AreStructurallyEqual(xml, xml2), $"结构不一致: {filePath}");
         }
     }
-} 
+}
---
+++ BannerlordModEditor.Common/Models/Data/CraftingTemplates.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace BannerlordModEditor.Common.Models.Data
+{
+    [XmlRoot("CraftingTemplates")]
+    public class CraftingTemplates
+    {
+        [XmlElement("CraftingTemplate")]
+        public List<CraftingTemplate> CraftingTemplateList { get; set; }
+
+        public bool ShouldSerializeCraftingTemplateList() => CraftingTemplateList != null && CraftingTemplateList.Count > 0;
+    }
+
+    public class CraftingTemplate
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("item_type")]
+        public string ItemType { get; set; }
+
+        [XmlAttribute("modifier_group")]
+        public string ModifierGroup { get; set; }
+
+        [XmlAttribute("item_holsters")]
+        public string ItemHolsters { get; set; }
+
+        [XmlAttribute("piece_type_to_scale_holster_with")]
+        public string PieceTypeToScaleHolsterWith { get; set; }
+
+        [XmlAttribute("hidden_piece_types_on_holster")]
+        public string HiddenPieceTypesOnHolster { get; set; }
+
+        [XmlAttribute("default_item_holster_position_offset")]
+        public string DefaultItemHolsterPositionOffset { get; set; }
+
+        [XmlAttribute("always_show_holster_with_weapon")]
+        public string AlwaysShowHolsterWithWeapon { get; set; }
+
+        [XmlAttribute("use_weapon_as_holster_mesh")]
+        public string UseWeaponAsHolsterMesh { get; set; }
+
+        [XmlElement("PieceDatas")]
+        public PieceDatas PieceDatas { get; set; }
+
+        [XmlElement("WeaponDescriptions")]
+        public WeaponDescriptions WeaponDescriptions { get; set; }
+
+        [XmlElement("StatsData")]
+        public List<StatsData> StatsDataList { get; set; }
+
+        [XmlElement("UsablePieces")]
+        public UsablePieces UsablePieces { get; set; }
+
+        public bool ShouldSerializePieceDatas() => PieceDatas != null;
+        public bool ShouldSerializeWeaponDescriptions() => WeaponDescriptions != null;
+        public bool ShouldSerializeStatsDataList() => StatsDataList != null && StatsDataList.Count > 0;
+        public bool ShouldSerializeUsablePieces() => UsablePieces != null;
+    }
+
+    public class PieceDatas
+    {
+        [XmlElement("PieceData")]
+        public List<PieceData> PieceDataList { get; set; }
+        public bool ShouldSerializePieceDataList() => PieceDataList != null && PieceDataList.Count > 0;
+    }
+
+    public class PieceData
+    {
+        [XmlAttribute("piece_type")]
+        public string PieceType { get; set; }
+
+        [XmlAttribute("build_order")]
+        public string BuildOrder { get; set; }
+    }
+
+    public class WeaponDescriptions
+    {
+        [XmlElement("WeaponDescription")]
+        public List<WeaponDescription> WeaponDescriptionList { get; set; }
+        public bool ShouldSerializeWeaponDescriptionList() => WeaponDescriptionList != null && WeaponDescriptionList.Count > 0;
+    }
+
+    public class WeaponDescription
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+    }
+
+    public class StatsData
+    {
+        [XmlAttribute("weapon_description")]
+        public string WeaponDescription { get; set; }
+
+        [XmlElement("StatData")]
+        public List<StatData> StatDataList { get; set; }
+        public bool ShouldSerializeStatDataList() => StatDataList != null && StatDataList.Count > 0;
+        public bool ShouldSerializeWeaponDescription() => !string.IsNullOrEmpty(WeaponDescription);
+    }
+
+    public class StatData
+    {
+        [XmlAttribute("stat_type")]
+        public string StatType { get; set; }
+
+        [XmlAttribute("max_value")]
+        public string MaxValue { get; set; }
+    }
+
+    public class UsablePieces
+    {
+        [XmlElement("UsablePiece")]
+        public List<UsablePiece> UsablePieceList { get; set; }
+        public bool ShouldSerializeUsablePieceList() => UsablePieceList != null && UsablePieceList.Count > 0;
+    }
+
+    public class UsablePiece
+    {
+        [XmlAttribute("piece_id")]
+        public string PieceId { get; set; }
+
+        [XmlAttribute("mp_piece")]
+        public string MpPiece { get; set; }
+
+        public bool ShouldSerializeMpPiece() => !string.IsNullOrEmpty(MpPiece);
+    }
+}
