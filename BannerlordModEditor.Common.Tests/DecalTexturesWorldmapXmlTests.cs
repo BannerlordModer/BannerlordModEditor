94ecdb31553c7ded32f138eabc93338c3af85168
ModerRAS <adqewsx@gmail.com>
2025-08-06 07:41:42 +0000

    适配 ModuleData XML 文件为强类型对象，添加对应 xUnit 测试，确保精确序列化行为

---
+++ BannerlordModEditor.Common.Tests/DecalTexturesWorldmapXmlTests.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using Xunit;
+using BannerlordModEditor.Common.Models.Data;
+
+namespace BannerlordModEditor.Common.Tests
+{
+    public class DecalTexturesWorldmapXmlTests
+    {
+        [Fact]
+        public void DecalTexturesWorldmap_RoundTrip_StructuralEquality()
+        {
+            var xmlPath = "BannerlordModEditor.Common.Tests/TestData/decal_textures_worldmap.xml";
+            var xml = File.ReadAllText(xmlPath);
+
+            var obj = XmlTestUtils.Deserialize<DecalTexturesWorldmap>(xml);
+            var xml2 = XmlTestUtils.Serialize(obj);
+
+            Assert.True(XmlTestUtils.AreStructurallyEqual(xml, xml2));
+        }
+    }
+}
