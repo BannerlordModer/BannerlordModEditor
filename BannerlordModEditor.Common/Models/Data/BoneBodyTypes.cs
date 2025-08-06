241a49e02070751669007b192efe7d0442f7112f
ModerRAS <adqewsx@gmail.com>
2025-08-06 09:13:08 +0000

    适配 ModuleData XML 文件为强类型对象，添加对应 xUnit 测试，确保精确序列化行为

--- BannerlordModEditor.Common/Models/Data/BoneBodyTypes.cs
+++ BannerlordModEditor.Common/Models/Data/BoneBodyTypes.cs
@@ -1,30 +1,35 @@
+using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 
-namespace BannerlordModEditor.Common.Models.Data;
-
-// bone_body_types.xml - Body part definitions for bone collision detection
-[XmlRoot("bone_body_types")]
-public class BoneBodyTypes
+namespace BannerlordModEditor.Common.Models.Data
 {
-    [XmlElement("bone_body_type")]
-    public List<BoneBodyType> BoneBodyType { get; set; } = new();
+    [XmlRoot("bone_body_types")]
+    public class BoneBodyTypes
+    {
+        [XmlElement("bone_body_type")]
+        public List<BoneBodyType> Items { get; set; } = new List<BoneBodyType>();
+    }
+
+    public class BoneBodyType
+    {
+        [XmlAttribute("type")]
+        public string Type { get; set; }
+
+        [XmlAttribute("priority")]
+        public int Priority { get; set; }
+
+        [XmlAttribute("activate_sweep")]
+        public bool? ActivateSweep { get; set; }
+
+        [XmlAttribute("use_smaller_radius_mult_while_holding_shield")]
+        public bool? UseSmallerRadiusMultWhileHoldingShield { get; set; }
+
+        [XmlAttribute("do_not_scale_according_to_agent_scale")]
+        public bool? DoNotScaleAccordingToAgentScale { get; set; }
+
+        public bool ShouldSerializeActivateSweep() => ActivateSweep.HasValue;
+        public bool ShouldSerializeUseSmallerRadiusMultWhileHoldingShield() => UseSmallerRadiusMultWhileHoldingShield.HasValue;
+        public bool ShouldSerializeDoNotScaleAccordingToAgentScale() => DoNotScaleAccordingToAgentScale.HasValue;
+    }
 }
-
-public class BoneBodyType
-{
-    [XmlAttribute("type")]
-    public string? Type { get; set; }
-
-    [XmlAttribute("priority")]
-    public string? Priority { get; set; }
-
-    [XmlAttribute("activate_sweep")]
-    public string? ActivateSweep { get; set; }
-
-    [XmlAttribute("use_smaller_radius_mult_while_holding_shield")]
-    public string? UseSmallerRadiusMultWhileHoldingShield { get; set; }
-
-    [XmlAttribute("do_not_scale_according_to_agent_scale")]
-    public string? DoNotScaleAccordingToAgentScale { get; set; }
-} 
